5e73aa2cb03afe74d32c472a54ab1e5726e4fd4e
ITstep <itstep@itstep.loc>
2022-02-21 21:50:55 +0600

    ticher pattern

--- app/Models/UserStuffs.php
+++ app/Models/UserStuffs.php
@@ -5,14 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Stuff extends Model
+class UserStuffs extends Model
 {
     use HasFactory;
-    protected $fillable = ['name'];
+    protected $fillable = ['user_id', 'stuff_id', 'quantity', ];
 
     public static function validatedData($request){
         $validated = $request->validate([
-            'name' => 'sometimes',
+            'user_id' => 'sometimes',
+            'stuff_id' => 'sometimes',
+            'quantity' => 'sometimes',
+
         ]);
 
         return $validated;
